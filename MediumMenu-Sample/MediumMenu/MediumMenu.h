9ea2af57b239593c8d23ce9f44ae0d38cffa7ef8
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2015-04-11 13:06:24 +0900

    Add target

---
+++ MediumMenu-Sample/MediumMenu/MediumMenu.h
@@ -0,0 +1,19 @@
+//
+//  MediumMenu.h
+//  MediumMenu
+//
+//  Created by Hiroki Nagasawa on 4/11/15.
+//  Copyright (c) 2015 pixyzehn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MediumMenu.
+FOUNDATION_EXPORT double MediumMenuVersionNumber;
+
+//! Project version string for MediumMenu.
+FOUNDATION_EXPORT const unsigned char MediumMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediumMenu/PublicHeader.h>
+
+
